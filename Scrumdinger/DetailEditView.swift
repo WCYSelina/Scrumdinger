0cc796f34ee62eedff19bb4979ca3f3f62c4044d
Selina <cwon0109@student.monash.edu>
2022-11-29 22:45:04 +1100

    Navigation 1.3.2

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,32 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Ching Yee Selina Wong on 29/11/2022.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    var body: some View {
+        Form{
+            Section(header: Text("Meeting Info")){
+                TextField("Title",text: $data.title)
+                HStack{
+                    Slider(value:$data.lengthInMinutes,in: 5...30,step: 1){
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
