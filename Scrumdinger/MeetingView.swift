adad62ba08b1223d0559910e4552f27a46e31337
Selina <cwon0109@student.monash.edu>
2022-12-05 22:51:12 +1100

    ***Section 7

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player:AVPlayer{AVPlayer.sharedDingPlayer}
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 16).fill(scrum.theme.mainColor)
@@ -21,6 +24,10 @@ struct MeetingView: View {
         }.padding()
             .onAppear{
                 scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+                scrumTimer.speakerChangedAction = {
+                    player.seek(to: .zero)
+                    player.play()
+                }
                     scrumTimer.startScrum()
             }
             .onDisappear{
