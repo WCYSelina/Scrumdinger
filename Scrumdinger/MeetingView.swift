edc0f34279a5d8f590234e462d1ae6c0889e3c3b
Selina <cwon0109@student.monash.edu>
2022-12-05 21:19:52 +1100

    ***Section 2

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -13,21 +13,6 @@ struct MeetingView: View {
         ZStack{
             RoundedRectangle(cornerRadius: 16).fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5,total: 15)
-                HStack{
-                    VStack (alignment: .leading){
-                        Text("Seconds Elapsed").font(.caption)
-                        Label("300",systemImage: "hourglass.bottomhalf.fill")
-                    }
-                    Spacer()
-                    VStack (alignment: .trailing){
-                        Text("Seconds Remaining").font(.caption)
-                        Label("600",systemImage: "hourglass.tophalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time Remaining")
-                .accessibilityValue("10 minutes")
                 Circle().strokeBorder(lineWidth: 24)
                 HStack{
                     Text("Speaker 1 of 3")
