06690cac5c542db25cd231fd9157903b7d474c80
Selina <cwon0109@student.monash.edu>
2022-12-05 21:36:40 +1100

    ***Section4

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 16).fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle().strokeBorder(lineWidth: 24)
                 HStack{
                     Text("Speaker 1 of 3")
